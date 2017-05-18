aa2921bdec5462d130e693f46b705b7417bfdf95
Desperado <244570238@qq.com>
2017-05-18 16:21:13 +0800

    Create Project

---
+++ recyclerviewlib/src/main/java/com/desperado/recyclerview/ArrowRefreshHeader.java
@@ -0,0 +1,188 @@
+package com.desperado.recyclerview;
+
+import android.animation.ValueAnimator;
+import android.content.Context;
+import android.graphics.drawable.AnimationDrawable;
+import android.os.Handler;
+import android.util.AttributeSet;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+
+public class ArrowRefreshHeader extends LinearLayout implements BaseRefreshHeader {
+
+	private LinearLayout mContainer;
+    private ImageView mPullRefreshView;
+	private int mState = STATE_NORMAL;
+
+	private static final int ROTATE_ANIM_DURATION = 180;
+
+	public int mMeasuredHeight;
+    private AnimationDrawable animationDrawable;
+
+	public ArrowRefreshHeader(Context context) {
+		super(context);
+		initView();
+	}
+
+	/**
+	 * @param context
+	 * @param attrs
+	 */
+	public ArrowRefreshHeader(Context context, AttributeSet attrs) {
+		super(context, attrs);
+		initView();
+	}
+
+	private void initView() {
+		// 初始情况，设置下拉刷新view高度为0
+		mContainer = (LinearLayout) LayoutInflater.from(getContext()).inflate(
+				R.layout.listview_header, null);
+        LayoutParams lp = new LayoutParams(LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
+        lp.setMargins(0, 0, 0, 0);
+		this.setLayoutParams(lp);
+        this.setPadding(0, 0, 0, 0);
+
+		addView(mContainer, new LayoutParams(LayoutParams.MATCH_PARENT, 0));
+		setGravity(Gravity.BOTTOM);
+        mPullRefreshView = (ImageView) findViewById(R.id.mPullRefreshView);
+        animationDrawable = (AnimationDrawable) mPullRefreshView.getDrawable();
+
+		measure(ViewGroup.LayoutParams.WRAP_CONTENT,ViewGroup.LayoutParams.WRAP_CONTENT);
+		mMeasuredHeight = getMeasuredHeight();
+	}
+
+	public void setState(int state) {
+		if (state == mState) return ;
+		switch(state){
+            case STATE_NORMAL:
+                if (mState == STATE_RELEASE_TO_REFRESH) {
+                }
+                if (mState == STATE_REFRESHING) {
+                }
+                break;
+            case STATE_RELEASE_TO_REFRESH:
+                if (mState != STATE_RELEASE_TO_REFRESH) {
+                }
+                break;
+            case     STATE_REFRESHING:
+                animationDrawable.start();
+                break;
+            case    STATE_DONE:
+                break;
+            default:
+		}
+		
+		mState = state;
+	}
+
+    public int getState() {
+        return mState;
+    }
+
+    @Override
+	public void refreshComplete(){
+        setState(STATE_DONE);
+        new Handler().postDelayed(new Runnable(){
+            public void run() {
+                reset();
+            }
+        }, 200);
+	}
+
+    public void setVisiableHeight(int height) {
+        if (height < 0)
+            height = 0;
+        LayoutParams lp = (LayoutParams) mContainer
+                .getLayoutParams();
+        lp.height = height;
+        mContainer.setLayoutParams(lp);
+    }
+
+
+    public void setVisibleHeight(int height) {
+		if (height < 0) height = 0;
+		LayoutParams lp = (LayoutParams) mContainer .getLayoutParams();
+		lp.height = height;
+		mContainer.setLayoutParams(lp);
+	}
+
+	public int getVisibleHeight() {
+        LayoutParams lp = (LayoutParams) mContainer.getLayoutParams();
+		return lp.height;
+	}
+
+    public int getVisiableHeight() {
+        int height = 0;
+        LayoutParams lp = (LayoutParams) mContainer
+                .getLayoutParams();
+        height = lp.height;
+        return height;
+    }
+    @Override
+    public void onMove(float delta) {
+        if(getVisibleHeight() > 0 || delta > 0) {
+            setVisiableHeight((int) delta + getVisiableHeight());
+            setVisibleHeight((int) delta + getVisibleHeight());
+            if (mState <= STATE_RELEASE_TO_REFRESH) { // 未处于刷新状态，更新箭头
+                if (getVisibleHeight() > mMeasuredHeight) {
+                    setState(STATE_RELEASE_TO_REFRESH);
+                }else {
+                    setState(STATE_NORMAL);
+                }
+            }
+        }
+    }
+
+    @Override
+    public boolean releaseAction() {
+        boolean isOnRefresh = false;
+        int height = getVisibleHeight();
+        if (height == 0) // not visible.
+            isOnRefresh = false;
+
+        if(getVisibleHeight() > mMeasuredHeight &&  mState < STATE_REFRESHING){
+            setState(STATE_REFRESHING);
+            isOnRefresh = true;
+        }
+        // refreshing and header isn't shown fully. do nothing.
+        if (mState == STATE_REFRESHING && height <=  mMeasuredHeight) {
+            //return;
+        }
+        if (mState != STATE_REFRESHING) {
+            smoothScrollTo(0);
+        }
+
+        if (mState == STATE_REFRESHING) {
+            int destHeight = mMeasuredHeight;
+            smoothScrollTo(destHeight);
+        }
+
+        return isOnRefresh;
+    }
+
+    public void reset() {
+        smoothScrollTo(0);
+        animationDrawable.stop();
+        new Handler().postDelayed(new Runnable() {
+            public void run() {
+                setState(STATE_NORMAL);
+            }
+        }, 500);
+    }
+
+    private void smoothScrollTo(int destHeight) {
+        ValueAnimator animator = ValueAnimator.ofInt(getVisibleHeight(), destHeight);
+        animator.setDuration(300).start();
+        animator.addUpdateListener(new ValueAnimator.AnimatorUpdateListener() {
+            @Override
+            public void onAnimationUpdate(ValueAnimator animation)
+            {
+                setVisibleHeight((int) animation.getAnimatedValue());
+            }
+        });
+        animator.start();
+    }
+}
